8c07054b3863ac578fd89dc9c125159610a53b14
AlSementsov <alsementsov@gmail.com>
2019-11-13 12:56:18 +0300

    add pethon script with Kalman instead of Median80

--- spo2_formula_test.py
+++
@@ -1,8 +0,0 @@
-def spo2_calc(IRmax,IRminl,IRminr,Rmax,Rminl,Rminr,A,B):
-    Red_DC=(Rminl+Rminr)/2
-    Red_AC = Rmax-Red_DC
-    IR_DC=(IRminl+IRminr)/2
-    IR_AC = IRmax-IR_DC
-    return (A-(B*((Red_AC/Red_DC)/(IR_AC/IR_DC))))
-
-print()
